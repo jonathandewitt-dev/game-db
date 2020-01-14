285a1bd9f6ea229fad2b1531eedfdcb367fda991
BDawgonnit <dewitt_bc@hotmail.com>
2020-01-14 03:02:25 +0000

    Added jest code snippet for VSCode

--- initvscode.sh
+++ initvscode.sh
@@ -102,3 +102,43 @@ cat > .vscode/launch.json << EOM
   ]
 }
 EOM
+
+touch .vscode/test.code-snippets
+cat > .vscode/test.code-snippets << EOM
+{
+  // https://code.visualstudio.com/docs/editor/userdefinedsnippets
+  //
+  // Place your game-db workspace snippets here. Each snippet is defined under a snippet name and has a scope, prefix, body and
+  // description. Add comma separated ids of the languages where the snippet is applicable in the scope field. If scope
+  // is left empty or omitted, the snippet gets applied to all languages. The prefix is what is
+  // used to trigger the snippet and the body will be expanded and inserted. Possible variables are:
+  // \$1, \$2 for tab stops, \$0 for the final cursor position, and \${1:label}, \${2:another} for placeholders.
+  // Placeholders with the same ids are connected.
+  // Example:
+  // "Print to console": {
+  //    "scope": "javascript,typescript",
+  //    "prefix": "log",
+  //    "body": [
+  //      "console.log('\$1');",
+  //      "\$2"
+  //    ],
+  //    "description": "Log output to console"
+  // }
+  //
+  "Add new test": {
+    "scope": "javascript,typescript",
+    "prefix": "describe",
+    "body": [
+      "describe('\${1:test}', () => {",
+      "  it('\${2:adds 1 + 1 to equal 2}', () => {",
+      "    expect.assertions(1)",
+      "    \${3:const result = 1 + 1}",
+      "    expect(\${4:result}).\${5:toBe(2)}",
+      "  })",
+      "})",
+      ""
+    ],
+    "description": "Add a new Jest test"
+  }
+}
+EOM
