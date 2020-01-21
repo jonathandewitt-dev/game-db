e65f93cc1cfe52524fb4a1c2d0040d70cca6caf5
BDawgonnit <dewitt_bc@hotmail.com>
2020-01-21 22:53:42 +0000

    Added CLI logic to main

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,3 +1,82 @@
-export default ({ env, args }: { env: string, args: string[] }): void => {
-  // TODO:
+import createUI from './ui/cli'
+import createDB from './db/volatile'
+import addGame from './domain/addGame'
+import viewGame from './domain/viewGame'
+import viewGamesForCollector from './domain/viewGamesForCollector'
+import inquirer from 'inquirer'
+
+export default async ({ env, args }: { env: string, args: string[] }): Promise<void> => {
+  const ui = await createUI()
+
+  const db = await createDB({
+    collectors: [
+      { id: 1, displayName: 'Game Collector 001', games: [1, 3, 4, 6] },
+    ],
+    games: [
+      { id: 1, title: 'Blaster Master' },
+      { id: 2, title: 'Super Mario Bros. 2' },
+      { id: 3, title: 'Kirby\'s Adventure' },
+      { id: 4, title: 'Ninja Gaiden' },
+      { id: 5, title: 'Shatterhand' },
+      { id: 6, title: 'Teenage Mutant Ninja Turtles' },
+    ],
+  })
+
+  const commandHandlers: any = {
+    viewGame: async () => {
+      const { id } = await inquirer.prompt([{
+        name: 'id',
+        message: 'Enter game id:',
+        type: 'number',
+        default: 0,
+      }])
+      return viewGame(ui.viewGame, db.getGame, id)
+    },
+
+    addGame: async () => {
+      const { title } = await inquirer.prompt([{
+        name: 'title',
+        message: 'Enter game title:',
+      }])
+      return addGame(ui.addGame, db.addGame, { title })
+    },
+
+    viewGamesForCollector: async () => {
+      const { collectorId, limit, firstId } = await inquirer.prompt([
+        { name: 'collectorId', message: 'Enter collectorId:', type: 'number' },
+        { name: 'limit', message: 'Enter limit:', type: 'number', default: 25 },
+        { name: 'firstId', message: 'Enter firstId:', type: 'number', default: 0 },
+      ])
+      return viewGamesForCollector(ui.viewGamesForCollector, db.getGamesForCollector, collectorId, {
+        limit,
+        firstId,
+      })
+    },
+  }
+  // @ts-ignore
+  const commands = Object.keys(commandHandlers).concat([new inquirer.Separator(), 'exit'])
+
+  let exit = false
+  while (!exit) {
+    try {
+      const { option } = await inquirer.prompt([{
+        name: 'option',
+        message: '\n\nSelect an option:',
+        type: 'list',
+        choices: commands,
+        default: 'exit',
+      }])
+
+      if (option === 'exit') {
+        console.log('exiting...')
+        exit = true
+        continue
+      }
+
+      const fn = commandHandlers[option]
+      console.log(await fn())
+    } catch (e) {
+      console.error(e)
+    }
+  }
 }
