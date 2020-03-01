c3b08f89c44234ec47f5e9a00ec510bfc74d26fa
jonathandewitt-dev <jonathan.c.dewitt@gmail.com>
2020-03-01 01:07:02 +0000

    removing volatile db

--- app/src/features/game/viewGame/viewGame.db.volatile.ts
+++
@@ -1,6 +0,0 @@
-import DBData from '../../../interfaces/DBData'
-import Game from '../../../interfaces/Game'
-
-export default async (data: DBData, id: number): Promise<Game> => {
-  return Promise.resolve(data.games.find(g => g.id === id))
-}
